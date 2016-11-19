6018834e15e97d7821f55b2e21f43acdeeb594e3
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-11-19 14:32:03 +0300

    вынесение класса точки

--- src/main/java/MyStrategy.java
+++ src/main/java/MyStrategy.java
@@ -260,37 +260,4 @@ public final class MyStrategy implements Strategy {
 
         return nearestTarget;
     }
-
-    /**
-     * Вспомогательный класс для хранения позиций на карте.
-     */
-    private static final class Point2D {
-        private final double x;
-        private final double y;
-
-        private Point2D(double x, double y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public double getX() {
-            return x;
-        }
-
-        public double getY() {
-            return y;
-        }
-
-        public double getDistanceTo(double x, double y) {
-            return StrictMath.hypot(this.x - x, this.y - y);
-        }
-
-        public double getDistanceTo(Point2D point) {
-            return getDistanceTo(point.x, point.y);
-        }
-
-        public double getDistanceTo(Unit unit) {
-            return getDistanceTo(unit.getX(), unit.getY());
-        }
-    }
 }
