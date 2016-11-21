9f527250dc286470ef2e1c0435219e529bbdcb18
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-11-21 16:47:00 +0300

    feature: увеличение безопасной зоны от базы противника и уменьшения радиуса моих крипов

--- src/main/java/Const.java
+++ src/main/java/Const.java
@@ -2,8 +2,8 @@ public class Const {
     public static final double LOW_HP_FACTOR = 0.4D;
     public static final double DIST_EPS = 0.01D;
     public static final double WARNING_DIST = 10.0D;
-    public static final double WARNING_DIST_TO_ENEMY_BASE = 1000.0D;
+    public static final double WARNING_DIST_TO_ENEMY_BASE = 950.0D;
     public static final int WAS_ATTACK_TIME = 60;
     public static final int HISTORY_CAPACITY = 100;
-    public static final double NEAR_DIST_INC = 100.0D;
+    public static final double NEAR_DIST_INC = 90.0D;
 }
