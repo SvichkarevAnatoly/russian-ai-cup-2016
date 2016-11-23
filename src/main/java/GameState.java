03ab8a63a60f3b773be528a87ac7822a8c772bb1
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-11-23 12:44:02 +0300

    refactor: удаление лишних состояний

--- src/main/java/GameState.java
+++ src/main/java/GameState.java
@@ -5,38 +5,21 @@ public class GameState {
     public boolean canNotMove;
 
     public boolean hasEnemy;
-    public boolean hasNearEnemyWizard;
-    public boolean hasNearEnemyMinions;
 
     public boolean hasMoreThanOneEnemy;
 
     public boolean canAttack;
-    public boolean canAttackWizard;
-    public boolean canAttackMinions;
 
     public boolean isUnderAttack;
-    public boolean isUnderTowerAttack;
-    public boolean isUnderWizardAttack;
 
     public boolean wasUnderAttack;
 
-    public boolean canBeUnderAttack;
     public boolean canBeUnderTowerAttack;
-    public boolean canBeUnderWizardAttack;
 
     public boolean isFriendMinionsAhead;
-    public boolean hasNearFriendWizard;
-    public boolean hasNearMoreThan2FriendWizard;
-    public boolean hasNearFriendMinions;
 
-    public boolean hasEnemyTowerLowHP;
-    public boolean hasFriendTowerLowHP;
-
-
-    public boolean isAttacking;
     public boolean isMoving;
     public boolean isOnlyTurning;
-    public boolean isRescueing;
 
     public boolean isNearEnemyBase;
     public boolean isOrkAtWarningDistance;
